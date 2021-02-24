0b713dcd168b8051d9222c7d38de0d025e7c8eaa
huangjun1314 <1776801625@qq.com>
2021-02-24 23:04:39 +0800

    发给电话

---
+++ jsonarrytest/src/main/java/com/java/jsonarrytest/JsonarrytestApplication.java
@@ -0,0 +1,13 @@
+package com.java.jsonarrytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonarrytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonarrytestApplication.class, args);
+	}
+
+}
---
+++ jsonarrytest/src/test/java/com/java/jsonarrytest/JsonarrytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.jsonarrytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonarrytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
